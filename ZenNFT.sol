e00a7d1065ff52f66a98b03e8839482f4944eb0e
sodhare-code <sodhare@gmail.com>
2025-09-26 03:27:05 +1200

    Update ZenNFT.sol

--- ZenNFT.sol
+++ ZenNFT.sol
@@ -3,19 +3,25 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract ZenNFT is ERC721URIStorage, Ownable {
-    uint256 public tokenCounter;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-    constructor() ERC721("ZenNFT", "ZNFT") {
-        tokenCounter = 0;
+    constructor() ERC721("Zenchain NFT", "ZENFT") {}
+
+    // Public mint function: caller pays gas and becomes owner of the token
+    function mint(string memory uri) public returns (uint256) {
+        _tokenIds.increment();
+        uint256 tokenId = _tokenIds.current();
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, uri);
+        return tokenId;
     }
 
-    function mintNFT(address recipient, string memory tokenURI) public returns (uint256) {
-        uint256 newItemId = tokenCounter;
-        _safeMint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-        tokenCounter += 1;
-        return newItemId;
+    // Optional: view total minted
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds.current();
     }
 }
